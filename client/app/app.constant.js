b700ef3fd8f54d11b047be7a466aa38475241b0f
D Shaffer <404shaff@gmail.com>
2016-06-06 13:48:17 -0400

    add bower cmpnnts

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("4vSubShopApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,19 @@
 'use strict';
 
-angular.module('4vSubShopApp', ['4vSubShopApp.auth', '4vSubShopApp.admin', '4vSubShopApp.constants',
-    'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
-    'validation.match'
+angular.module('4vSubShopApp', [
+                  '4vSubShopApp.auth',
+                  '4vSubShopApp.admin',
+                  '4vSubShopApp.constants',
+                  'ngCookies',
+                  'ngResource',
+                  'ngSanitize',
+                  'btford.socket-io',
+                  'ui.router',
+                  'ui.bootstrap',
+                  'validation.match',
+                  'ngAnimate'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
-
     $locationProvider.html5Mode(true);
   });
